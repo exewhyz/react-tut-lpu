7044051be8941a6a4318c7c2794c134ed3d081df
Aniket Raj <araj52444@gmail.com>
2026-01-28 10:58:48 +0530

    till components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Main from "./components/main";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main/>
+      <Footer />
+    </div>
+  );
+}
+
+// export const name = "Aniket";
+// export const age = 2;
+
+// export default App;
+
+// export { name, age }
