0abb383b1d615062c0b038c8903207383fed6748
Aniket Raj <araj52444@gmail.com>
2026-01-28 16:20:32 +0530

    till form handlelling

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
 
-
 const Main = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState();
 
+  const increment = (c) => {
+    // setCount(count + c);
+    setCount((prev) => prev + c);
+  };
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    increment(value);
+  }
   return (
-    <div>Main</div>
-  )
-}
+    <div>
+      count: {count}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="incrementer">Incrementer:</label>
+        <input 
+          id="incrementer"
+          type="number"
+          required
+          value={value}
+          onChange={(e)=> setValue(Number(e.target.value))}
+        />
+        <button>Increment</button>
+        {/* <button onClick={() => increment(value)}>Increment</button> */}
+      </form>
+    </div>
+  );
+};
 
 export default Main;
